5367fedb9475c0956f8a9ac734d1dc3258b026a8
ValentinaCorreaG <valentina.correa@urosario.edu.co>
2026-02-13 11:41:11 -0500

    add env variable for npm installation

--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -93,6 +93,14 @@ module.exports = {
                     elevated: "#FFFFFF",
                     inverse: "#202122",
                 },
+                'pastel-blue': {
+                    50: '#eff6ff',
+                    100: '#dbeafe',
+                    200: '#bfdbfe',
+                    300: '#93c5fd',
+                    400: '#60a5fa',
+                    500: '#3b82f6',
+                }
             },
         },
     },
